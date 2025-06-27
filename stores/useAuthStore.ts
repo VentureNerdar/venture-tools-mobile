ea733854e6c7f3663ae0d3acfe137d045eeb6039
Arkar <arkarchanmyaeko@gmail.com>
2025-06-27 17:43:44 +0700

    fix user device bug

--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -108,7 +108,7 @@ export const useAuthStore = defineStore("auth", () => {
       }
 
       // Register device with backend
-      await $fetch(`${config.public.apiURL}users/id/${userId}/devices/`, {
+      await $fetch(`${config.public.apiURL}users/id/${userId}/devices`, {
         method: "POST",
         body: {
           device_id: deviceId,
